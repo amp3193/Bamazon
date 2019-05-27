abe9212a585b84c8fb06fd1be31ea6b627daa0a6
amp3193 <amp3193@yahoo.com>
2019-05-27 12:54:56 -0400

    api routes working. seed data scripts added and working. images added to image folder

---
+++ scripts/seed.js
@@ -0,0 +1,10 @@
+const db = require("../models");
+const products = require("./products.json");
+
+db.sequelize.sync({ force: true }).then(function () {
+    return db.Product.bulkCreate(products);
+}).then(function () {
+    db.sequelize.close();
+}).catch(function (err) {
+    console.log(err);
+});
