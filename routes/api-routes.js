6349e7fe4c8fb14d3f4e2b362541a9c33bd64732
amp3193 <amp3193@yahoo.com>
2019-05-28 17:37:32 -0400

    added image data to seed file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -24,42 +24,18 @@ module.exports = function(app) {
       });
   });
 
-  //examples in case I need
-  // // POST route for saving a new todo
-  // app.post("/api/todos", function(req, res) {
-  //   // create takes an argument of an object describing the item we want to
-  //   // insert into our table. In this case we just we pass in an object with a text
-  //   // and complete property
-  //   db.Todo.create({
-  //     text: req.body.text,
-  //     complete: req.body.complete
-  //   }).then(function(dbTodo) {
-  //     // We have access to the new todo as an argument inside of the callback function
-  //     res.json(dbTodo);
-  //   });
-  // });
-
-
-
-  // // PUT route for updating todos. We can get the updated todo data from req.body
-  // app.put("/api/todos", function(req, res) {
-  //   // Update takes in an object describing the properties we want to update, and
-  //   // we use where to describe which objects we want to update
-  //   db.Todo.update({
-  //     text: req.body.text,
-  //     complete: req.body.complete
-  //   }, {
-  //     where: {
-  //       id: req.body.id
-  //     }
-  //   }).then(function(dbTodo) {
-  //     res.json(dbTodo);
-  //   });
-  // });
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+  app.put("/api/products", function(req, res) {
+    db.Product.update({
+      stock_quantity: req.body.stock_quantity,
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbProduct) {
+      res.json(dbProduct);
+    });
+  });
 
 };
-
-
-
-//**********************
-// TODO fill in with bamazon stuff
